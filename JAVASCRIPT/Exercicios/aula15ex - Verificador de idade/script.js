ce8649a46f00fc584344b7270198cee25f6ba543
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-18 09:06:45 -0300

    ada

--- JAVASCRIPT/Exercicios/aula15ex - Verificador de idade/script.js
+++ JAVASCRIPT/Exercicios/aula15ex - Verificador de idade/script.js
@@ -1,1 +1,15 @@
-alert('Olá')
+
+
+msg.innerHTML(`O ano é ${ano}`)
+
+function calcidade() {
+    var data = new Date
+    var ano = data.getFullYear
+    var fano = document.getElementById('nasc')
+    var res = document.querySelector('div#res')
+
+    if (fano.ariaValueMax.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente')
+    }
+
+}
