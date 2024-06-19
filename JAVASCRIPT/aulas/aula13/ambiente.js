6aa2d76b524a47e3810cacd331663d7da7d05c65
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-19 20:44:11 -0300

    Repetições

---
+++ JAVASCRIPT/aulas/aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+    //Executa primeiro e depois faz o teste
+    do {
+        console.log(`Passo ${c}`)
+        c++ // c = c + 1
+    } while (c <= 6)
+
+    // Testa Primeiro e depois executa
+    var c = 1
+    while (c <= 3 ) {
+        console.log(`Passo ${c}`)
+        c++ // c = c + 1
+    } 
