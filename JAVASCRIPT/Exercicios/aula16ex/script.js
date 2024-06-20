a99ad0420d6d97c1dad8c91a251a562f37971935
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-20 19:51:55 -0300

    Exercicio Finalizado

--- JAVASCRIPT/Exercicios/aula16ex/script.js
+++ JAVASCRIPT/Exercicios/aula16ex/script.js
@@ -1,5 +1,3 @@
-
-
 function contagem() {
     var inicio = document.getElementById('inicio')
     var fim = document.getElementById('fim')
@@ -9,6 +7,22 @@ function contagem() {
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('Erro, faltam dados!')
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando... '
+        let i = Number(inicio.value) //pega o valor(.value) dentro de inicio e converte para numero(Number)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            //Contagem Crescente
+            for(let c = i;c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        } else {
+            //Contagem Decrescente
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` 
+        
     }
 }
