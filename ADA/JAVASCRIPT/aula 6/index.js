8bbca1709764dd2c868f9d4ab7cf65444b740e10
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-21 20:19:00 -0300

    ADA - 6

---
+++ ADA/JAVASCRIPT/aula 6/index.js
@@ -0,0 +1,20 @@
+/* 
+todo  Utilize sempre o Camel case no JavaScript
+todo-----------------------------------------------------------
+*     JavaScript possui tipagem dinâmica: infere os tipos de dados
+*     JavaScript possui uma tipagem fraca, facilmente mistura textos com números      
+*/
+
+var nome = "Luciano Reis"; //!| string (texto): utilize sempre aspas
+var idade = 27; //!| number
+var altura = 1.77; //!| number
+var estudando = false; //! boolean (booleano: verdadeiro ou falso)
+
+console.log(nome);
+console.log(nome, typeof nome);
+console.log(idade);
+console.log(idade, typeof idade);
+console.log(altura);
+console.log(altura, typeof altura);
+console.log(estudando);
+console.log(estudando, typeof estudando);
