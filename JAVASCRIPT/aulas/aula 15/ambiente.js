3c4440c72a457af3fdefdb1a9b47e2527ab4d137
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-20 21:05:33 -0300

    Variaveis Compostas

---
+++ JAVASCRIPT/aulas/aula 15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9, 3];
+num.sort();
+num.push(1);
+console.log(`Nosso vetor é o ${num}`);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do vetor é ${num[1]}`);
+
+/*
+    let num = [5,8,4]
+    num[3] = 6 - cria uma nova caixa no array e adiciona o elemento(6)
+    .push() - adiciona mais um elemento na ultima vaga da caixa
+    .length - atributo que conta quantos elementos tem no Array(num)
+    .sort() - coloca o vetor em ordem
+*/
