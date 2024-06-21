ea989e1447f1346350a8d69e5267eba27691426c
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-20 21:37:15 -0300

    Aula 15 Finalizada

--- JAVASCRIPT/aulas/aula 15/ambiente.js
+++ JAVASCRIPT/aulas/aula 15/ambiente.js
@@ -5,10 +5,14 @@ console.log(`Nosso vetor é o ${num}`);
 console.log(`O vetor tem ${num.length} posições`);
 console.log(`O primeiro valor do vetor é ${num[1]}`);
 
+let pos = num.indexOf(8)
+console.log(`O valor está na posição ${pos}`)
+
 /*
     let num = [5,8,4]
     num[3] = 6 - cria uma nova caixa no array e adiciona o elemento(6)
     .push() - adiciona mais um elemento na ultima vaga da caixa
     .length - atributo que conta quantos elementos tem no Array(num)
     .sort() - coloca o vetor em ordem
+    .indexOF()   
 */
