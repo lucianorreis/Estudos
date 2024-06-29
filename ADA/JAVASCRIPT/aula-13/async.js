4bbe9aa6d29ac17dff85a9d25271dbd0ee8e8273
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-29 19:49:04 -0300

    async

--- ADA/JAVASCRIPT/aula-13/async.js
+++ ADA/JAVASCRIPT/aula-13/async.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 //*| 1. Callbacks
 
 // Função assíncrona (ler um arquivo leva tempo)
-fs.readFile("aula-13/arquivo.txt", (erro, conteudoDoArquivo) => {
+fs.readFile("aula-13/dados.txt", (erro, conteudoDoArquivo) => {
    if (erro) {
     console.log("Ocorreu um erro ao tentar ler o arquivo:", erro);
    } else {
