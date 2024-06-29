a2ebc16d3133483da7a4394a9722b2c5e8f93c05
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-29 19:00:51 -0300

    Aula 12 - Reduce

--- ADA/JAVASCRIPT/aula 12/reduce.js
+++ ADA/JAVASCRIPT/aula 12/reduce.js
@@ -1,9 +1,20 @@
 //?| Reduce: Método que executa (para cada elemento o array) a função que foi pasada como parâmetro, resultando em um elemento único.
 const numeros = [1, 2, 3, 4, 5]
 
-const soma = numeros.reduce((acumulador, elemento) => {
+const media = numeros.reduce((acumulador, elemento, _, arrayCompleto) => {
     console.log(acumulador)
-    return elemento + acumulador;
+    return (elemento / arrayCompleto.length) + acumulador;
 }, 0);
 
-console.log(soma);
+console.log(media);
+
+const somaDosPares = numeros.reduce((acumulador, numero) => {
+    if (numero % 2 === 0) {
+        return numero + acumulador;
+    } else {
+        return acumulador;
+    }
+}, 0)
+
+console.clear()
+console.log(somaDosPares)
