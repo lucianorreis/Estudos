040bebf6a9975cc4bbc3d603ed0333423505067a
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-29 18:39:21 -0300

    aula 12 - filter

--- ADA/JAVASCRIPT/aula 12/filter.js
+++ ADA/JAVASCRIPT/aula 12/filter.js
@@ -1,3 +1,9 @@
 //?| Filter
+//?| map sempre retorna um array de mesmo tamanho do array original
+//?| filter: sempre retorna um array de tamanho igual ou inferior ao tamanho do array original
 
 const valores = [39,45,32,24,4,22,2,49,23, 8]
+
+const pares = valores.filter((valor) => valor % 2 === 0)
+
+console.log(pares)
