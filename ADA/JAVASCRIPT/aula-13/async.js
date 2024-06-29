1fb254169114ab87b3218fdef6bf46c8e71df6e5
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-29 19:59:14 -0300

    async

--- ADA/JAVASCRIPT/aula-13/async.js
+++ ADA/JAVASCRIPT/aula-13/async.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //*| 1. Callbacks
-
+/*
 // Função assíncrona (ler um arquivo leva tempo)
 fs.readFile("ADA/JAVASCRIPT/aula-13/dados.txt", (erro, conteudoDoArquivo) => {
    if (erro) {
@@ -10,3 +10,12 @@ fs.readFile("ADA/JAVASCRIPT/aula-13/dados.txt", (erro, conteudoDoArquivo) => {
     console.log(String(conteudoDoArquivo))
    }
 })
+*/
+
+//?| Exemplo 2: setTimeout (função que cria um timer)
+
+console.log('Antes do SetTimeout')
+
+setTimeout(() => {
+    console.log("Isso aqui vai ser executado apos 3 segundos!")
+}, 3 * 1000)
