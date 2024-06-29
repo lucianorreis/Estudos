7a0ff446a4ec20664c22e06ea7b26e9d609445ad
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-29 19:54:26 -0300

    async

--- ADA/JAVASCRIPT/aula-13/tempCodeRunnerFile.js
+++ ADA/JAVASCRIPT/aula-13/tempCodeRunnerFile.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 //*| 1. Callbacks
 
 // Função assíncrona (ler um arquivo leva tempo)
-fs.readFile("ADA/JAVASCRIPT/aula-13/dados.txt", (erro, conteudoDoArquivo) => {
+fs.readFile("aula-13/dados.txt", (erro, conteudoDoArquivo) => {
    if (erro) {
     console.log("Ocorreu um erro ao tentar ler o arquivo:", erro);
    } else {
-    console.log(String(conteudoDoArquivo))
+    console.log(conteudoDoArquivo)
    }
 })
