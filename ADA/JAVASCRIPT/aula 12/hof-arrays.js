0e278b797f15dd1814a45d0d0b9e5c72c794fa53
Luciano.Reis <lrr.arms233@gmail.com>
2024-06-28 21:13:08 -0300

    Aula 12

--- ADA/JAVASCRIPT/aula 12/hof-arrays.js
+++ ADA/JAVASCRIPT/aula 12/hof-arrays.js
@@ -1,5 +1,37 @@
 //?| Array.prototype.every(): Verificar se todos os elementos de um array seguem uma determinada condição retornada pela função
 
+const numeros = [40, 16, 34, 67, 89, 23, 10]
 
+const pessoas = [
+    {
+        nome: 'Pessoa',
+        idade: 34,
+        altura: 1.77,
+    },
+    {
+        nome: 'Pessoa 2',
+        idade: 25,
+        altura: 1.56,
+    },
+    {
+        nome: 'Pessoa 3',
+        idade: 46,
+        altura: 1.82,
+    },
+    ];
 
-//?| Some
+const todosPositivos = numeros.every((elemento) => elemento > 0)
+
+console.log(todosPositivos)
+
+const MaiorIdade = pessoas.every((pessoa) => pessoa.idade > 18)
+
+console.log(MaiorIdade)
+
+//?| Array.prototype.Some(): Verificar se algum elemento do Array torna verdadeira uma determinada condição retornada pela função
+
+const numeros2 = [-1, 3,7,-3, 5]
+
+const retorno = numeros2.some((numero) => numero > 0)
+
+console.log(retorno)
