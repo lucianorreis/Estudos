6633b0c034b1c94f60efa5ab567677aeffea0e73
Luciano.Reis <lrr.arms233@gmail.com>
2024-07-22 21:23:03 -0300

    CM01A04

---
+++ Estudonauta/C/modulo 1/m01a04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    printf("O %s tem %i anos\n","Luciano", 20);
+    printf("Seu peso atual é de %6.2fKg\n", 53.5); //Print só mostra strings
+    printf("O seu sexo é %c", 'M');
+}
+/*
+    Int[4] - short[2], long[8], signed, unsigned
+        1835, 134, -18       ¦
+    Float[4]
+        4.5, 0.0075, -1.0893 ¦
+    Char
+        "Isso é um Char"     ¦
+    Double[8]
+        o dobro de Float
+    Void
+        valor vazio
+
+*/
