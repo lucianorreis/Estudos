4c300b13bab29e2fd37b2ebb4bde2c8c02739f57
Luciano.Reis <lrr.arms233@gmail.com>
2024-07-28 19:21:17 -0300

    Arrays

--- ADA/JAVASCRIPT/aula 7/arrays.js
+++ ADA/JAVASCRIPT/aula 7/arrays.js
@@ -1,6 +1,6 @@
 let numeros = [1, 2, 3, 4, 5, 6];
 let numerosPares = numeros.filter(function(numero) {
-    return numero % 1 === 0;
+    return numero % 2 === 0;
 });
 
 console.log(numerosPares);
