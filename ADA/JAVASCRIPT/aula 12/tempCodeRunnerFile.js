850c973b7fee7806dc99e04f1f9e22edcdc031bd
Luciano.Reis <lrr.arms233@gmail.com>
2024-07-30 21:21:39 -0300

    update

--- ADA/JAVASCRIPT/aula 12/tempCodeRunnerFile.js
+++ ADA/JAVASCRIPT/aula 12/tempCodeRunnerFile.js
@@ -1,9 +1,21 @@
-let lista1 = [1, 2, 3, 4, 5, 6, 7];
-let lista2 = [1, 2, 3, 4, 5, 6, 7.5];
+const prompt = require("readline-sync");
 
-function identicidade() {
-    let identicos = lista1 === lista2;
-    console.log(identicos);
+let lista1 = [];
+let lista2 = [];
+
+function verificador() {
+    encheLista(prompt.question('Digite uma série de números: '))
+    if (lista1.every() === lista2.every()) {
+        console.log("Ambas as listas são iguais")
+    } else {
+        console.log("Ambas as listas são diferentes")
+    }
+}
+function encheLista(mensagem) {
+  do {
+    lista1.push((mensagem));
+  } while (isNaN === lista1 && lista1.length <= 5);
 }
 
-identicidade();
+
+verificador()
